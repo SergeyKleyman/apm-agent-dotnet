d4a6ca459a107adb7122fc85e4dad9671b035f2b
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2019-09-12 23:01:47 +0300

    Remove redundant imports in LazyContextualInitTests

--- test/Elastic.Apm.Tests/HelpersTests/LazyContextualInitTests.cs
+++ test/Elastic.Apm.Tests/HelpersTests/LazyContextualInitTests.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
 using Elastic.Apm.Helpers;
 using Elastic.Apm.Tests.Extensions;
 using Elastic.Apm.Tests.TestHelpers;
